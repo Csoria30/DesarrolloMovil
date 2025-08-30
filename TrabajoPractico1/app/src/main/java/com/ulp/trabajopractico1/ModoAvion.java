e837843a8acda52a0126e9469394e189a9a51eb2
Soria Cristian <cristianjsoria.29@gmail.com>
2025-08-30 13:44:43 -0300

    Creacion de clase ModoAcion (BroadcastReceiver)

---
+++ TrabajoPractico1/app/src/main/java/com/ulp/trabajopractico1/ModoAvion.java
@@ -0,0 +1,26 @@
+package com.ulp.trabajopractico1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.Toast;
+
+public class ModoAvion extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        boolean avionState = intent.getBooleanExtra("state", false);
+
+        if(avionState){
+            Toast.makeText(context, "Modo avion habilitado", Toast.LENGTH_SHORT).show();
+            Intent llamada = new Intent(Intent.ACTION_DIAL);
+            llamada.setData(Uri.parse("tel:2664553747"));
+            llamada.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(llamada);
+        }else{
+            Toast.makeText(context, "Modo avion deshabilitado", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
