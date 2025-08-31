2317c442639df349228744d238f3bf0832e46acf
Soria Cristian <cristianjsoria.29@gmail.com>
2025-08-30 23:14:11 -0300

    Implementacion de iconos en la vista MainActivity

--- TrabajoPractico2/app/src/main/java/com/ulp/trabajopractico2/View/MainActivity.java
+++ TrabajoPractico2/app/src/main/java/com/ulp/trabajopractico2/View/MainActivity.java
@@ -1,10 +1,13 @@
 package com.ulp.trabajopractico2.View;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.TypedValue;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -28,6 +31,22 @@ public class MainActivity extends AppCompatActivity {
 
         EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
+
+        // MODIFICAR EL TAMAÑO DEL ICONO
+        // Obtenemos el drawable
+        Drawable lupa = ContextCompat.getDrawable(this, R.drawable.lupa);
+
+        // Ajustamos tamaño del drawable (en píxeles)
+        int anchoIcono = 20;
+        int altoIcono = 20;
+        int ancho = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, anchoIcono, getResources().getDisplayMetrics());
+        int alto  = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, altoIcono, getResources().getDisplayMetrics());
+        lupa.setBounds(0, 0, ancho, alto);
+
+        // Asignamos el drawable al inicio del EditText
+        binding.etLibroBuscado.setCompoundDrawables(lupa, null, null, null);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tvAutorLibro), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
