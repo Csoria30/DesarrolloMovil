f20c33167edd98dbb7a0efbc0f15cdb2ce7ae597
Soria Cristian <cristianjsoria.29@gmail.com>
2025-09-15 21:03:07 -0300

    Mostrando Lat y Log mediante Toast

--- ProyectosClase/EjemploLocalizacion/app/src/main/java/com/ulp/ejemplolocalizacion/MainActivity.java
+++ ProyectosClase/EjemploLocalizacion/app/src/main/java/com/ulp/ejemplolocalizacion/MainActivity.java
@@ -1,24 +1,57 @@
 package com.ulp.ejemplolocalizacion;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
+    private MainActivityViewModel viewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        solicitarPermisos();
+        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainActivityViewModel.class);
+
+        viewModel.getMLocation().observe(this, new Observer<Location>() {
+            @Override
+            public void onChanged(Location location) {
+
+
+                if (location != null) {
+                    Toast.makeText(MainActivity.this, "Lat: " + location.getLatitude() + " Long: " + location.getLongitude(), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Ubicación no disponible", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
+        viewModel.obtenerUbicacion();
+
     }
+
+    public void solicitarPermisos(){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M
+                && (checkSelfPermission(ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED)  ||
+                (checkSelfPermission(ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED)){
+            requestPermissions(new String[]{ACCESS_FINE_LOCATION,ACCESS_COARSE_LOCATION},1000);
+        }
+    }
+
+
 }
