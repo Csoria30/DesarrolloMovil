9ac130bf99602b37b3d8e43c81c24ec091ba8c39
Soria Cristian <cristianjsoria.29@gmail.com>
2025-09-26 21:10:49 -0300

    ProductoModelo - Se agregan propiedades - Creacion de constructor - Creacion de getters y setters

---
+++ TrabajoPractico3/app/src/main/java/com/ulp/trabajopractico3/Model/ProductoModelo.java
@@ -0,0 +1,37 @@
+package com.ulp.trabajopractico3.Model;
+
+public class ProductoModelo {
+    private String codigo;
+    private String descripcion;
+    private double precio;
+
+    public ProductoModelo(String codigo, String descripcion, double precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
