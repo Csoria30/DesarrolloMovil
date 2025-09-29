fe5982ba186bdd21d5f8e62dc9bd2133b237e889
Soria Cristian <cristianjsoria.29@gmail.com>
2025-09-29 19:59:31 -0300

    Fead: Eliminar Producto - Se implementa logica para validar producto existente. - Se agrega validacion al formulario del Fragmento Eliminar. - Verifica que el producto exista en la lista stock.

--- TrabajoPractico3/app/src/main/java/com/ulp/trabajopractico3/ui/eliminar/EliminarFragment.java
+++ TrabajoPractico3/app/src/main/java/com/ulp/trabajopractico3/ui/eliminar/EliminarFragment.java
@@ -11,28 +11,72 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.lifecycle.Observer;
 import com.ulp.trabajopractico3.R;
+import com.ulp.trabajopractico3.databinding.FragmentEliminarBinding;
+import com.ulp.trabajopractico3.model.ProductoModel;
 
 public class EliminarFragment extends Fragment {
-
-    private EliminarViewModel mViewModel;
+    private FragmentEliminarBinding binding;
+    private EliminarViewModel viewModel;
 
     public static EliminarFragment newInstance() {
         return new EliminarFragment();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_eliminar, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(EliminarViewModel.class);
+        binding = FragmentEliminarBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        //Observer: Botop
+        binding.btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.buscarProducto(binding.etCodigoEliminar.getText().toString());
+            }
+        });
+
+        //Observer: Msge Error
+        viewModel.getMsgeError().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String error) {
+                Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Observer: Ok
+        viewModel.getProductoEncontrado().observe(getViewLifecycleOwner(), new Observer<ProductoModel>() {
+            @Override
+            public void onChanged(ProductoModel producto) {
+
+                Bundle bundle = new Bundle();
+                bundle.putString("codigo", producto.getCodigo());
+                bundle.putString("descripcion", producto.getDescripcion());
+                bundle.putDouble("precio", producto.getPrecio());
+
+            }
+        });
+
+        //Observer: Limpiar Campos del formulario
+        viewModel.getCampoCodigo().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String codigo) {
+                binding.etCodigoEliminar.setText(codigo);
+            }
+        });
+
+        return root;
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(EliminarViewModel.class);
-        // TODO: Use the ViewModel
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 
 }
--- TrabajoPractico3/app/src/main/java/com/ulp/trabajopractico3/ui/eliminar/EliminarViewModel.java
+++ TrabajoPractico3/app/src/main/java/com/ulp/trabajopractico3/ui/eliminar/EliminarViewModel.java
@@ -1,7 +1,63 @@
 package com.ulp.trabajopractico3.ui.eliminar;
 
+import static com.ulp.trabajopractico3.MainActivity.stock;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.ulp.trabajopractico3.model.ProductoModel;
+
 public class EliminarViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<String> msgeError;
+    private MutableLiveData<ProductoModel> productoEncontrado;
+    private MutableLiveData<String> campoCodigo;
+
+    public LiveData<String> getMsgeError() {
+        if(msgeError == null)
+            msgeError = new MutableLiveData<>();
+        return msgeError;
+    }
+
+    public LiveData<ProductoModel> getProductoEncontrado() {
+        if(productoEncontrado == null)
+            productoEncontrado = new MutableLiveData<>();
+        return productoEncontrado;
+    }
+
+    public LiveData<String> getCampoCodigo() {
+        if(campoCodigo == null)
+            campoCodigo = new MutableLiveData<>();
+        return campoCodigo;
+    }
+
+    public void buscarProducto(String codigo) {
+        if(codigo.isEmpty()) {
+            msgeError.setValue("Debe ingresar un codigo");
+            return;
+        }
+
+        if(!codigo.matches("\\d+")) {
+            msgeError.setValue("El codigo debe contener solo numeros");
+            return;
+        }
+
+        // Buscar producto
+        ProductoModel encontrado = null;
+
+        for(ProductoModel producto : stock) {
+            if(producto.getCodigo().equalsIgnoreCase(codigo)) {
+                encontrado = producto;
+                break;
+            }
+        }
+
+        //Mensaje Error
+        if(encontrado != null){
+            productoEncontrado.setValue(encontrado);
+            msgeError.setValue("Producto Encontrado");
+        }else{
+            msgeError.setValue("Producto no encontrado");
+        }
+    }
 }
